07bed8b2c03ab770212c55cd155c19636c67b57d
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2017-01-13 00:38:02 +0000

    add linked list with add and show

---
+++ LinkedList/HarshdeepLinkedList/LinkedList.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class LinkedList {
+	Node head;
+	Node original_node;
+
+	public LinkedList(){
+		head = null; // Initially the node will be null
+	}
+
+	public void add_node(int number){
+		Node new_node = new Node(number);
+
+		if(head == null){
+			head = new_node;
+			return;
+		}
+
+		// original_node is basically acting like as a temp node
+		Node current_node = head;
+		while(current_node != null){
+			original_node = current_node;
+			current_node = current_node.pointer_next_node;
+		}
+		original_node.pointer_next_node = new_node;
+	}
+
+	public void show_list(){
+		Node current_node = head;
+		while(current_node != null){
+			System.out.println(current_node.data);
+			current_node = current_node.pointer_next_node;
+		}
+	}
+
+	// Have to add the deletion method
+	public static void main(String[] args){
+		LinkedList my_linked_list = new LinkedList();
+		my_linked_list.add_node(5);
+		my_linked_list.add_node(4);
+		my_linked_list.add_node(3);
+
+		my_linked_list.show_list();
+	}
+}
---
+++ LinkedList/HarshdeepLinkedList/Node.java
@@ -0,0 +1,11 @@
+public class Node{
+
+	/* Node should have data and reference to the next node */
+	int data;
+	Node pointer_next_node;
+
+	public Node(int number){
+		data = number;
+		pointer_next_node = null;
+	}
+}
